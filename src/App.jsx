ed716a4f8d4b7145a5a739882473eb8e41216746
Cristhiamcaceres97 <133033840+Cristhiamcaceres97@users.noreply.github.com>
2023-07-04 22:43:14 -0600

    solucion app

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
-import Inicio from "./Components/Inicio";
-import Carousel from "./Components/Carousel";
-import Buscador from "./Components/Buscador";
+import Home from "./Components/Home";
+import Help from "./Components/Help";
+import Clients from "./Components/Clients";
+import BookDetails from "./Components/BookDetails";
 import BookList from "./Components/BookList";
 import PaymentMethods from "./Components/PaymentMethods";
-<<<<<<< HEAD
 import ListComponent from "./Components/ListComponent";
-=======
-import ListComponent from "./Components/ListComponent"; 
->>>>>>> b1db40d5e27ee7e88c3b88b77df96334bad4803e
-import BookDetails from "./Components/BookDetails";
 
 const App = () => {
-  return (        
-    <Router>      
+  return (
+    <Router>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/carousel" element={<Carousel />} />
-        <Route path="/buscador" element={<Buscador />} />
-        <Route path="/booklist" element={<BookList />} />
-        <Route path="/paymentmethods" element={<PaymentMethods />} />
-        <Route path="/ListComponent" element={<ListComponent />} />
-        <Route path="/bookDetails/:index" element={<BookDetails />} />
-      </Routes>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/ayuda" component={Help} />
+        <Route path="/clientes" component={Clients} />
+        <Route path="/book/:id" component={BookDetails} />
+        <Route path="/books" component={BookList} />
+        <Route path="/payment" component={PaymentMethods} />
+        <Route path="/list" component={ListComponent} />
+      </Switch>
     </Router>
   );
 };
